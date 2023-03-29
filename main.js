bb472e8416080c239404b8c22c6b37c50cbe2444
valeriaparra123 <101677456+valeriaparra123@users.noreply.github.com>
2023-03-28 18:11:23 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var cocossd = "";
+
+webcam(300, 450);
+
+function setup()
+{
+    createCanvas(450, 300, 0, 0, )
+}
+
+function start()
+{
+    modelLoad(cocossd);
+    if(cocossd = true)
+    {
+        results("Estado:detectando objetos");
+    }
+}
+
+function modelLoaded()
+{
+    if(status = true)
+    {
+    console.log("Modelo cargado");
+    }
+}
+
+function draw()
+{
+    canvas = webcam;
+}
